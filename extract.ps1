fd707109c745a926d359711ceab30d1064009278
Florian <florian.fiminhac@stordata.fr>
2014-05-23 15:35:50 +0200

    no message

---
+++ extract.ps1
@@ -0,0 +1,20 @@
+﻿$asupv2="C:\ASUPV2"
+$destination="D:\_Stordata\traitement-cv"
+$7z="$asupv2\7za.exe"
+
+ $directory=dir -Path $destination -Directory
+ foreach ($dir in $directory){
+
+
+do {
+
+$archives=Get-Item -Path $destination\$dir\* -Include *.cab
+
+foreach ($archive in $archives.Name)
+{
+& "$7z" x "$destination\$dir\$archive" -o"$destination\$dir" -aoa
+Remove-Item "$destination\$dir\$archive"
+}}
+while ($flagArchive=(Test-Path -Path $destination\$dir\* -Include *.cab))
+
+}
